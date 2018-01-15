1413c5e814edfc7d96dbc6dc1ed77a5a870c7f89
myd <myd@myddeiMac.local>
2018-01-15 11:40:21 +0800

    本地提交

---
+++ TestPro/NieyinlongView.h
@@ -0,0 +1,13 @@
+//
+//  NieyinlongView.h
+//  TestPro
+//
+//  Created by myd on 2018/1/15.
+//  Copyright © 2018年 myd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NieyinlongView : UIView
+
+@end
---
+++ TestPro/NieyinlongView.m
@@ -0,0 +1,29 @@
+//
+//  NieyinlongView.m
+//  TestPro
+//
+//  Created by myd on 2018/1/15.
+//  Copyright © 2018年 myd. All rights reserved.
+//
+
+#import "NieyinlongView.h"
+
+@implementation NieyinlongView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+
+
+@end
