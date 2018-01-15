c2eca2d1abe2170865e7d939c098d78fd2b57fb2
myd <myd@myddeiMac.local>
2018-01-15 11:47:11 +0800

    s

---
+++ TestPro/Mine/MineViewController.h
@@ -0,0 +1,13 @@
+//
+//  MineViewController.h
+//  TestPro
+//
+//  Created by myd on 2018/1/15.
+//  Copyright © 2018年 myd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MineViewController : UIViewController
+
+@end
--- TestPro/ViewController.m
+++ TestPro/ViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"第一行新增");
 }
 
